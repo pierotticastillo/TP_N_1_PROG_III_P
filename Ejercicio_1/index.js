c0da40527e996b5e1a6471174fb6c9638c1ce98e
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2024-08-29 00:14:56 -0300

    Separación de las funciones en otro archivo

--- Ejercicio_1/index.js
+++ Ejercicio_1/index.js
@@ -1,178 +1,19 @@
-// Config
-const urlApiThrones = 'https://thronesapi.com/api/v2/characters'
-
-const fs = require("fs");
-const promises = require("fs/promises");
-
-
-// 1. Realizar una función que permita recuperar la información del personaje “Ned Stark”.
-
-const getCharacter = async (fullName) => {
-    try {
-        const response = await fetch(urlApiThrones);
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-
-        const datos = await response.json();
-
-        let captura = datos.find(personaje => personaje.fullName == String(fullName));
-
-        if (captura) {
-            console.log(captura)
-        }
-        else {
-            console.log(`El personaje con el nombre ${fullName} no fue encontrado`);
-        }
-
-
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-
-// 2. Realizar una función que permita recuperar todos los personajes disponibles.
-// 3. Persistir el resultado de la segunda consulta localmente en un archivo JSON.
-const getAll = async () => {
-    try {
-        const response = await fetch(urlApiThrones);
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-        const datos = await response.json();
-        console.log(datos);
-
-        try {
-            await fs.promises.access('./personajes.json', fs.constants.F_OK);
-            console.log("El archivo *personajes.json* ya existe");
-        }
-        catch (error) {
-            if (error.code === 'ENOENT') {
-
-                const datosParseados = JSON.stringify(datos, null, 2);
-                await fs.promises.writeFile("./personajes.json", datosParseados);
-                console.log("Los personajes fueron guardados en el archivo *personajes.json*");
-            }
-        }
-
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-// 4. Leer el archivo local de personajes
-const readFile = async () => {
-    try {
-        const characters = await promises.readFile("./personajes.json", "utf-8")
-        const datosParseados = JSON.parse(characters)
-        console.log(datosParseados)
-    }
-    catch (error) {
-        console.error(error)
-    }
-}
-
-// a. Mostrar por consola los personajes de la familia Stark. Es decir: “family” = “House Stark”.
-const getFamily = async (family) => {
-    try {
-
-        const datos = await promises.readFile("./personajes.json");
-
-        const datosParseados = JSON.parse(datos)
-
-        const captura = datosParseados.filter(personaje => personaje.family == String(family))
-
-        if (datosParseados && captura) {
-            console.log(`Los personajes de la familia ${family} son:`);
-            captura.forEach(personaje => {
-                console.log(personaje)
-            })
-        }
-        else {
-            console.log(`No se encontraron personajes para la famlia ${family}`)
-        }
-
-    }
-    catch (error) {
-        console.error(error)
-    }
-
-}
-
-// b. Agregar un nuevo personaje y sobrescribir el archivo original.
-
-const createCharacter = async (character) => {
-    try {
-
-        const datos = await promises.readFile("./personajes.json");
-
-        const personajesParseados = JSON.parse(datos)
-
-        const ultimoID = personajesParseados[personajesParseados.length - 1].id
-
-        const newCharacter = {
-            "id": parseInt(ultimoID + 1),
-            "firstName": character.firstName,
-            "lastName": character.lastName,
-            "fullName": character.fullName,
-            "title": character.title,
-            "family": character.family,
-        }
-
-        personajesParseados.push(newCharacter)
-
-        const datosParseados = JSON.stringify(personajesParseados, null, 2)
-
-        await promises.writeFile("./personajes.json", datosParseados)
-
-        console.log(`El personaje fue creado exitosamente, sus datos son:\nid: ${ultimoID + 1}'\nFirst Name:${character.firstName}\nLast Name: ${character.lastName}\nFullname:${character.fullName}\nTitle Family:${character.family}`)
-    }
-    catch (error) {
-        console.error(error)
-    }
-
-};
-
-// c. Eliminar los personajes cuyo ID sean mayores a 25 y sobrescribir el archivo original.
-
-const removeGreaterThan = async (id) => {
-    try {
-        const datos = await promises.readFile("./personajes.json");
-
-        const personajesParseados = JSON.parse(datos)
-
-        let capturaPersonajes = personajesParseados.filter(personaje => personaje.id <= parseInt(id));
-
-        const datosParseados = JSON.stringify(capturaPersonajes, null, 2)
-
-        await promises.writeFile("./personajes.json", datosParseados)
-
-        console.log("Los personajes fueron eliminados exitosamente")
-
-    }
-    catch (error) {
-        console.error(error)
-    }
-}
-
+const funciones = require("./funciones.js");
 // TESTEO
 
 async function main() {
     // 1
-    await getCharacter("Ned Stark")
+    await funciones.getCharacter("Ned Stark")
 
     // 2 & 3
-    await getAll()
+    await funciones.getAll()
 
 
     // 4
-    await readFile()
+    await funciones.readFile()
 
     // a
-    await getFamily("House Stark")
+    await funciones.getFamily("House Stark")
 
     // b
     const newCharacter = {
@@ -183,12 +24,12 @@ async function main() {
         "family": "Italiana",
     }
 
-    await createCharacter(newCharacter)
+    await funciones.createCharacter(newCharacter)
 
 
     // c
 
-    // await removeGreaterThan(25)
+    // await funciones.removeGreaterThan(25)
 
 
 }
--- Ejercicio_2/index.js
+++ Ejercicio_2/index.js
@@ -1,120 +1,14 @@
-const urlApiProducts = 'https://fakestoreapi.com/products'
-
-// 1. Recuperar la información de todos los productos (products).
-const getAll = async () => {
-    try {
-        const response = await fetch(urlApiProducts);
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-
-        const datos = await response.json();
-        console.log(datos);
-
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-
-// 2. Recuperar la información de un número limitado de productos (products).
-const getLimited = async (limit) => {
-    try {
-        if (limit <= 0 || limit == undefined) {
-            throw new Error("Esta consulta no es posible");
-        }
-
-        const response = await fetch(`${urlApiProducts}?limit=${parseInt(limit)}`);
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-
-        const datos = await response.json();
-        console.log(datos);
-
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-
-// 3. Agregar un nuevo producto (product).
-const createProduct = async (addProduct) => {
-    try {
-        const response = await fetch(urlApiProducts, {
-            method: "POST", headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(addProduct)
-        });
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-
-        const idProducto = await response.json();
-
-        console.log(`Los datos del producto agregado son:\nID: ${idProducto.id}\nTitulo: ${addProduct.title}\nPrecio: ${addProduct.price}\nDescripción: ${addProduct.description}\nCategoria: ${addProduct.category}`);
-
-
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-
-// 4. Retornar un producto (product) según un “id” como parámetro.
-const getById = async (id) => {
-    try {
-        const response = await fetch(`${urlApiProducts}/${parseInt(id)}`);
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-
-        const datos = await response.json();
-        console.log(datos);
-
-
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-// 5. Eliminar un producto (product).
-const deleteProduct = async (id) => {
-    try {
-        const response = await fetch(`${urlApiProducts}/${id}`, {
-            method: "DELETE", headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error("Error", response.status);
-        }
-
-        const datos = await response.json();
-        console.log(`Los datos del producto eliminado son:\nID: ${datos.id}\nTitulo: ${datos.title} \nPrecio: ${datos.price} \nDescripción: ${datos.description} \nCategoria: ${datos.category}`);
-
-
-    } catch (error) {
-        console.error(error);
-    };
-}
-
-
 // TESTEO
+
+const funciones = require("./funciones.js")
 
 const main = async () => {
     // 1
 
-    await getAll();
+    await funciones.getAll();
 
     // 2
-    await getLimited(9);
+    await funciones.getLimited(9);
 
 
     // 3
@@ -125,17 +19,16 @@ const main = async () => {
         image: 'https://i.pravatar.cc',
         category: 'electronic'
     };
-
-    await createProduct(newProduct);
+// 
+    await funciones.createProduct(newProduct);
 
 
     // 4
-    await getById(18);
+    await funciones.getById(18);
 
     // 5
-    await deleteProduct(9)
+    await funciones.deleteProduct(9)
 
 }
 
 main()
-
